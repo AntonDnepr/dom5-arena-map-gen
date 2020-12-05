b58cfeb90a7cd9978a2c71a2d0afbb95bd85a40c
Anton Zhyrney <a.zhyrney@gmail.com>
2020-12-05 18:41:40 +0200

    fix: fixed file

--- apps/domdata/parser.py
+++ apps/domdata/parser.py
@@ -6,5 +6,5 @@ def parse_units():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     with open(os.path.join(current_dir, "csvs/BaseU.csv"), "r") as csv_file:
         reader = csv.DictReader(csv_file)
-    for line in reader:
-        print(line)
+        for line in reader:
+            print(line)
