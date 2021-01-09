5d7db76c98aec7f661637216e4e3632b7bcbe725
Anton Zhyrney <a.zhyrney@gmail.com>
2021-01-09 18:25:37 +0200

    fix: removed redundant data

--- conf/settings.py
+++ conf/settings.py
@@ -239,7 +239,6 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
-    "EXCEPTION_HANDLER": "apps.core.custom_exceptions.custom_exception_handler",
 }
 
 SIMPLE_JWT = {
